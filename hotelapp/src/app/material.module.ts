4005cd714dba62baf71a66562e5fae3ca8ce1ba2
Angyal Zsolt <angyal.zsolt@hotmail.hu>
2018-11-02 17:11:03 +0100

    sidebar

--- hotelapp/src/app/material.module.ts
+++ hotelapp/src/app/material.module.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 
 
 @NgModule({
-	imports: [MatBadgeModule, MatButtonModule],
-	exports: [MatBadgeModule, MatButtonModule]
+	imports: [
+		MatBadgeModule,
+		MatButtonModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatSidenavModule,
+		MatListModule
+	 ],
+	exports: [
+		MatBadgeModule,
+		MatButtonModule,
+		MatToolbarModule,
+		MatIconModule,
+		MatSidenavModule,
+		MatListModule
+		]
 })
 
 export class MaterialModule {}
